b6c4881b9a29aa451e5c4461a15606087ec140ae
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-05-26 10:01:20 +0800

    fix portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import data from "../api/data.json";
+import { Modal, Button } from "react-bootstrap";
 
 const Portfolio = () => {
   const [filteredPort, setFilteredPort] = useState(data.portfolio);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const portfolioAll = () => {
     setFilteredPort(data.portfolio);
@@ -59,22 +64,18 @@ const Portfolio = () => {
               >
                 <div className="portfolio-wrap">
                   <img
-                    // src={`%PUBLIC_URL%/assets/img/portfolio/${portfolio.img}`}
-                    // src={require(`../../public/assets/img/portfolio/${portfolio.img}`)}
                     src={require(`../portfolio-imgs/${portfolio.img}`)}
                     className="img-fluid"
                     alt="true"
+                    onClick={handleShow}
                   />
+
                   <div className="portfolio-links">
                     <a
-                      href={`assets/img/portfolio/${portfolio.img}`}
-                      data-gallery="portfolioGallery"
-                      className="portfolio-lightbox"
-                      title="App 1"
+                      href={portfolio.url}
+                      title="More Details"
+                      target="_blank"
                     >
-                      <i className="bx bx-plus" />
-                    </a>
-                    <a href="portfolio-details.html" title="More Details">
                       <i className="bx bx-link" />
                     </a>
                   </div>
@@ -83,6 +84,21 @@ const Portfolio = () => {
             ))}
           </div>
         </div>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </section>
       {/* End Portfolio Section */}
     </>
