dcce578770f352d8fb137437bd95d59d67a20a9e
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-05-29 09:36:43 +0800

    added basename to browser router

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
-    <HashRouter>
+    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
       <App />
-    </HashRouter>
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
