3f8174583b9bf9b762e5b38aa0ba2206cd8b3cc5
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-05-29 09:15:49 +0800

    added hashrouter to index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </BrowserRouter>
   </React.StrictMode>
 );
